bebb176094963c75f512afaab489777fbe95b568
warren-wacko <warren.jacob.abdon1@gmail.com>
2025-12-05 14:36:41 +0800

    Fix test-db route

--- salindugo_backend/index.js
+++ salindugo_backend/index.js
@@ -43,6 +43,16 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.send(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Database connection failed");
+  }
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT || 5000}`);
 });
