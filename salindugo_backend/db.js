6a38368fbdd2513ada3352298c61219726c7927e
warren-wacko <warren.jacob.abdon1@gmail.com>
2025-12-05 14:44:14 +0800

    script add

--- salindugo_backend/db.js
+++ salindugo_backend/db.js
@@ -5,7 +5,10 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
