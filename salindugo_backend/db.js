5f640bad87292422b0a70c39a5677b1cb85831ff
warren-wacko <warren.jacob.abdon1@gmail.com>
2025-12-05 14:34:29 +0800

    Connect backend to Supabase with DATABASE_URL

--- salindugo_backend/db.js
+++ salindugo_backend/db.js
@@ -4,11 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  database: process.env.DATABASE_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 export default pool;
