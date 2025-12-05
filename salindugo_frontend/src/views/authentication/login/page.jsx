d71826147e5321dcfd188efa9a012dcd8f130d5c
warren-wacko <warren.jacob.abdon1@gmail.com>
2025-12-05 15:26:48 +0800

    fixed url

--- salindugo_frontend/src/views/authentication/login/page.jsx
+++ salindugo_frontend/src/views/authentication/login/page.jsx
@@ -83,14 +83,17 @@ export default function LoginPage() {
     setSuccessMessage("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          email: formData.email,
-          password: formData.password,
-        }),
-      });
+      const res = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/auth/login`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            email: formData.email,
+            password: formData.password,
+          }),
+        }
+      );
 
       const data = await res.json();
 
@@ -153,7 +156,7 @@ export default function LoginPage() {
 
     try {
       const res = await fetch(
-        "http://localhost:5000/api/auth/forgot-password",
+        `${import.meta.env.VITE_API_URL}/api/auth/forgot-password`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
