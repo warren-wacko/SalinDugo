9cc49161debbc7bb7d6fd11cc29a0dbed7063410
wackoshen <warren.jacob.abdon1@gmail.com>
2026-03-31 01:44:50 +0800

    Fixed some url endpoint mismatch

--- salindugo_frontend/src/views/authentication/login/page.jsx
+++ salindugo_frontend/src/views/authentication/login/page.jsx
@@ -83,14 +83,17 @@ export default function LoginPage() {
     setSuccessMessage("");
 
     try {
-      const res = await fetch(`http://localhost:5000/api/auth/login`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          email: formData.email,
-          password: formData.password,
-        }),
-      });
+      const res = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/auth/login`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            email: formData.email,
+            password: formData.password,
+          }),
+        },
+      );
 
       const data = await res.json();
 
@@ -153,12 +156,12 @@ export default function LoginPage() {
 
     try {
       const res = await fetch(
-        `http://localhost:5000/api/auth/forgot-password`,
+        `${import.meta.env.VITE_API_URL}/api/auth/forgot-password`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ email: resetEmail }),
-        }
+        },
       );
       const data = await res.json();
 
