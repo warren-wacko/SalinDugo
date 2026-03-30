a86b697e07f121278626060ec9f05661137a8e2e
wackoshen <warren.jacob.abdon1@gmail.com>
2026-03-31 01:58:46 +0800

    fix

--- salindugo_ml_engine/forecast_service.py
+++ salindugo_ml_engine/forecast_service.py
@@ -13,7 +13,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://salin-dugo.vercel.app/",  
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
