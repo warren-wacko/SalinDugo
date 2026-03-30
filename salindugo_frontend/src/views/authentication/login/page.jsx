08815a7490014897c301f1502b936d940347ff4d
wackoshen <warren.jacob.abdon1@gmail.com>
2026-03-31 02:27:02 +0800

    added debugging

--- salindugo_frontend/src/views/authentication/login/page.jsx
+++ salindugo_frontend/src/views/authentication/login/page.jsx
@@ -95,6 +95,9 @@ export default function LoginPage() {
         },
       );
 
+      const text = await res.text();
+      console.log("RESPONSE:", text);
+
       const data = await res.json();
 
       if (res.ok) {
